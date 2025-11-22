a24ef8248107d2f9f44a1574234520d5379dba29
alexandrappe <00117222@uca.edu.sv>
2025-11-21 19:29:00 -0600

    feat(ui): update login styles and add color/typography variables :lipstick:

--- learnsy/src/pages/login/login.jsx
+++ learnsy/src/pages/login/login.jsx
@@ -31,58 +31,58 @@ const Login = () => {
     };
 
     return (
-        <div className="min-h-screen w-full flex justify-center items-center bg-[#F5F5F5] px-4">
+        <div className="min-h-screen w-full flex justify-center items-center bg-white px-4">
 
-            {/* TARJETA PRINCIPAL */}
-            <div className="bg-white w-full max-w-xl rounded-[30px] p-12 shadow-xl border border-gray-200 relative">
+            {/* Tarjeta principal - contenedor del login */}
+            <div className="bg-white w-full max-w-xl rounded-2xl p-12 shadow-xl border border-gray-300 relative">
 
-                {/* IMAGEN DECORATIVA (NUEVOS CÍRCULOS) */}
+                {/* Logo */}
                 <img
                     src="/src/assets/circulos.png"
                     alt="Learnsy"
                     className="w-28 absolute top-6 left-1/2 -translate-x-1/2"
                 />
 
-                {/* TÍTULO */}
-                <h1 className="text-center text-3xl font-bold text-[#4B2FA9] mt-32">
+                {/* Titulo */}
+                <h1 className="text-center text-3xl title font-normal mt-28 mb-2">
                     ¡HOLA DE NUEVO!
                 </h1>
 
-                <p className="text-center text-gray-500 text-sm mb-10">
+                <p className="text-center body-font font-medium text-sm text-[var(--color-dark-purple)] mb-10">
                     Ingresa tus datos para empezar
                 </p>
 
-                {/* FORMULARIO */}
+                {/* Formulario */}
                 <form onSubmit={handleSubmit} className="flex flex-col gap-6">
 
                     <div className="flex flex-col w-full">
-                        <label className="text-sm font-semibold text-gray-700 mb-1">Nombre</label>
+                        <label className="text-sm font-semibold body text-medium mb-1">Usuario o correo</label>
                         <input
                             type="text"
                             value={nameOrEmail}
                             onChange={(e) => setNameOrEmail(e.target.value)}
                             placeholder="correo@correo.com"
-                            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#C65BCF]"
+                            className="w-full title-font font-light text-[var(--color-border-shadow)] placeholder:text-gray-400 border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#C65BCF]"
                         />
                     </div>
 
                     <div className="flex flex-col w-full">
-                        <label className="text-sm font-semibold text-gray-700 mb-1">Contraseña</label>
+                        <label className="text-sm font-semibold body text-medium mb-1">Contraseña</label>
                         <input
                             type="password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                             placeholder="**************"
-                            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#C65BCF]"
+                            className="w-full title-font font-light text-[var(--color-border-shadow)]  placeholder:text-gray-400 border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-400 "
                         />
                     </div>
 
                     <button
                         type="submit"
                         disabled={isLoading}
-                        className="bg-[#C65BCF] text-white font-semibold py-2 rounded-lg mt-4 hover:bg-[#b152c0] transition"
+                        className="btn-primary py-2.5 rounded-lg mt-4 transition"
                     >
-                        {isLoading ? 'Ingresando...' : 'INICIAR SESIÓN'}
+                        {isLoading ? 'Ingresando...' : 'INICIAR SESION'}
                     </button>
                 </form>
             </div>
