67adcdf876788a060cc34694a588d2642a50da36
Joma203 <00120422@uca.edu.sv>
2025-11-22 11:53:36 -0600

    add: Cambios en colores y posiciones de circulos en landing page

--- learnsy/src/pages/landing/landing.jsx
+++ learnsy/src/pages/landing/landing.jsx
@@ -25,11 +25,11 @@ const Landing = () => {
       </nav>
 
       {/* CÍRCULOS RESPONSIVE */}
-      <div className="absolute left-0 top-32 md:top-24 w-24 h-24 md:w-48 md:h-48 bg-[#9C3AD8] rounded-full -translate-x-1/2 hidden sm:block"></div>
+      <div className="absolute left-0 top-32 md:top-24 w-24 h-24 md:w-48 md:h-48 bg-[#92247C] rounded-full -translate-x-1/2 hidden sm:block"></div>
 
-      <div className="absolute left-0 -bottom-10 md:-bottom-20 w-20 h-20 md:w-40 md:h-40 bg-[#6D5CD4] rounded-full -left-10 md:-left-20"></div>
+      <div className="absolute left-0 -bottom-10 md:-bottom-20 w-20 h-20 md:w-40 md:h-40 bg-[#5A4598] rounded-full -left-10 md:-left-0"></div>
 
-      <div className="absolute right-0 top-1/2 -translate-y-1/2 w-20 h-20 md:w-36 md:h-36 bg-[#CF97D9] rounded-full translate-x-1/2 hidden sm:block"></div>
+      <div className="absolute right-0 top-1/2 -translate-y-1/2 w-20 h-20 md:w-36 md:h-36 bg-[#CF92C6] rounded-full translate-x-1/2 hidden sm:block"></div>
 
       {/* CONTENIDO */}
       <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 h-auto lg:h-[calc(100vh-100px)] items-center px-6 md:px-10 py-10 lg:py-0 relative z-10 gap-10">
@@ -59,7 +59,7 @@ const Landing = () => {
         <div className="flex justify-center lg:justify-start pr-0 lg:pr-10">
           <img
             src="http://localhost:5173/src/assets/images/young-hispanic-student-girl-smiling-happy-university.png"
-            className="w-full max-w-[380px] md:max-w-[500px] lg:h-screen lg:w-auto object-cover rounded-lg lg:rounded-none"
+            className="w-full max-w-[380px] md:max-w-[500px] lg:h-full lg:w-auto object-cover rounded-lg lg:rounded-none"
           />
         </div>
 
