8b4bf10a4beb5c0ccd0fe844b869f548354328a5
MarcoGArana <00037822@uca.edu.sv>
2025-11-23 15:18:09 -0600

    fix: image problem

--- learnsy/src/pages/layout/layout.jsx
+++ learnsy/src/pages/layout/layout.jsx
@@ -3,6 +3,7 @@ import { Outlet, Link, Navigate, useLocation, useParams } from "react-router-dom
 import { useAuthStore } from "../../store/authStore";
 import NavItem from "../../components/layout/navItem/navItem";
 import { BiLogOutCircle } from "react-icons/bi";
+import logo from "../../../src/assets/OpcionLogo2.PNG";
 
 const Layout = () => {
   const [asideState, setAsideState] = useState('close');
@@ -76,7 +77,7 @@ const Layout = () => {
               <svg id="menu" xmlns="http://www.w3.org/2000/svg" height="24px" className="scale-150" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" /></svg>
             </button>
             <Link to="/dashboard">
-              <img src="http://localhost:5173/src/assets/OpcionLogo2.PNG" className="max-w-[16.5rem] h-auto" />
+              <img src={logo} className="max-w-[16.5rem] h-auto" />
             </Link>
           </li>
           <li>
